aa7d5989f04377e96ed7ddc5bf33c7a115469749
Brennan <brennanterre@gmail.com>
2023-03-28 14:36:15 +0800

    corrections

--- insert.php
+++ insert.php
@@ -1,28 +1,18 @@
 <?php
-$connection = mysqli_connect("localhost","root","");
-$db = mysqli_select_db($connection, 'usa_jerhel');
+require_once("connect.php");
 
+if (isset($_POST)) {
+   $name = $_POST['name'];
+   $email = $_POST['email'];
+   $comment = $_POST['comment'];
 
-if(isset($_POST['submit']))
-{
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $comment = $_POST['comment'];
+   $query = "INSERT INTO contents (name, email, comment) VALUES ('$name', '$email', '$comment')";
+   $query_run = mysqli_query($connection, $query);
 
-
-    $query = "INSERT INTO contents (`name`, `email`, `comment`) VALUES ('$name', '$email', '$comment')";
-    $query_run = mysqli_query($connection, $query);
-
-if ($query_run)
-{
-    echo '<script> alert ("COMMENTED"); </script>';
-    header('Location: index.php');
+   if ($query_run) {
+      echo '<script> alert ("COMMENTED"); </script>';
+      header('Location: index.php');
+   } else {
+      echo '<script> alert("ERROR"); </script>';
+   }
 }
-else{echo '<script> alert("ERROR"); </script>';
-
-}
-}
-
-
-
-?>
